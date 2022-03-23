971ba9e0ab61245829989e0c9959c47f209e0892
dallen67 <102166327+dallen67@users.noreply.github.com>
2022-03-22 21:13:34 -0400

    Update serial52.py

--- serial52.py
+++ serial52.py
@@ -15,44 +15,36 @@ def sendData(data, port, baud):
     data += "\r\n"
     ser.write(data.encode()) 
 
-def receiveData(): #Function still needs reworking to solve errors, Drew Allen
-    ser = serial.Serial()
-    ser.port = ""
-    ser.baudrate = 115200
+def receiveData(port, baud): #Function still needs reworking to solve errors, Drew Allen
+    ser = serial.Serial(port, baud) 
+
     ser.timeout = 0
     line = ser.readline()
-    print(line)
-
-if __name__ == "__main__":
-    spObject = serial.tools.list_ports.comports()
-    serialPorts = []
-    for i in spObject:
-    	serialPorts.append(i.device)
-    print('Available serial ports:')
-    for i in range(len(serialPorts)):
-        print('%i. %s' %(i+1,serialPorts[i])) 
-    selectedPort = int(input('Please select a port: '))
-    while selectedPort-1 not in range(len(serialPorts)):
-        print('Invalid input')
-        selectedPort = input('Please select a port: ')
-    tty = serialPorts[selectedPort-1] 
-    data = input('Please enter the data you want to send over the serial port: ')
-	
-    t1 = threading.Thread(target=sendData(data, tty, 115200))
-    t2 = threading.Thread(target=receiveData())
-	
-    t1.start()
-    t2.start()
-    
-    t1.join()
-    t2.join()
-    print("Done!")
-
-
+    print(line.decode())
 def main():
-    
-    sendData()
-	
-    receiveData()
+    if __name__ == "__main__":
+        spObject = serial.tools.list_ports.comports()
+        serialPorts = []
+        for i in spObject:
+            serialPorts.append(i.device)
+        print('Available serial ports:')
+        for i in range(len(serialPorts)):
+            print('%i. %s' %(i+1,serialPorts[i])) 
+        selectedPort = int(input('Please select a port: '))
+        while selectedPort-1 not in range(len(serialPorts)):
+            print('Invalid input')
+            selectedPort = input('Please select a port: ')
+        tty = serialPorts[selectedPort-1] 
+        data = input('Please enter the data you want to send over the serial port: ')
+        
+        t1 = threading.Thread(target=sendData(data, tty, 115200))
+        #t2 = threading.Thread(target=receiveData("COM6", 115200))
+        
+        t1.start()
+        #t2.start()
+        
+        t1.join()
+        #t2.join()
+        print("Done!")
 
 main()
