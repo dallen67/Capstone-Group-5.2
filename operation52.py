1e58e208069fdb20f1c25994fcd7ad521dd5cf46
dallen67 <102166327+dallen67@users.noreply.github.com>
2022-04-14 19:10:38 -0400

    Update operation52.py

--- operation52.py
+++ operation52.py
@@ -9,74 +9,84 @@ import threading
 #Test different way of handling processes
 from concurrent.futures import ProcessPoolExecutor
 
-def sendData(data, port, baud):
-    time.sleep(1)
-    with serial.Serial(port=port, baudrate=baud, timeout=1) as ser:   
-        ser.reset_input_buffer()
-        ser.reset_output_buffer()
-        ser.flushInput()
-        ser.flushOutput()
-        
-        data += ''
-        print()
-        print("Data that was sent: ", data)    
-        ser.write(data.encode())
-        ser.close()
+class Timer:
 
-def receiveData(port, baud):
-  
-    time.sleep(1)
-    with serial.Serial(port=port, baudrate=baud, timeout=1) as ser:
-        ser.reset_input_buffer()
-        ser.reset_output_buffer()               
-        ser.flushInput()
-        ser.flushOutput()
-        
-        line = ser.read_until(expected=b'!END!')
-        print()
-        print("Data that was received")             
-        print(line.decode())
-        ser.close()
+    def sendData(data, port, baud, self):
+        time.sleep(1)
+        with serial.Serial(port=port, baudrate=baud, timeout=1) as ser:   
+            ser.reset_input_buffer()
+            ser.reset_output_buffer()
+            ser.flushInput()
+            ser.flushOutput()
 
-def main():
+            data += ''
+            print()
+            print("Data that was sent: ", data)    
+            ser.write(data.encode())
+            bt = time.time()
+            self._start_time = time.perf_cunter()
+            print("Current Time:", bt)
+            
+            ser.close()
 
-    if __name__ == "__main__":
-        spObject = serial.tools.list_ports.comports()
+    def receiveData(port, baud, self):
 
-        serialPorts = []
-        print()
-        for i in spObject:
-            serialPorts.append(i.device)
-        print('Available ports:')
-        for i in range(len(serialPorts)):
-            print('%i. %s' % (i+1, serialPorts[i]))
-        selectedPort = int(input('Please select a port to send data to: '))
-        while selectedPort-1 not in range(len(serialPorts)):
-            print('Invalid input')
-            selectedPort = input('Please select a port: ')
-        tty = serialPorts[selectedPort-1]
-        data = input(
-            'Please enter the data you want to send over the serial port: ')
-        print()
+        time.sleep(1)
+        with serial.Serial(port=port, baudrate=baud, timeout=1) as ser:
+            ser.reset_input_buffer()
+            ser.reset_output_buffer()               
+            ser.flushInput()
+            ser.flushOutput()
 
-        spObject2 = serial.tools.list_ports.comports()
-        serialPorts2 = []
-        for i in spObject2:
-            serialPorts2.append(i.device)
-        print('Available ports:')
-        for i in range(len(serialPorts2)):
-            print('%i. %s' % (i+1, serialPorts2[i]))
-        selectedPort2 = int(input('Please select a port to listen on: '))
-        while selectedPort2-1 not in range(len(serialPorts2)):
-            print('Invalid input')
-            selectedPort2 = input('Please select a port: ')
+            line = ser.read_until(expected=b'!END!')
+            print()
+            print("Data that was received")             
+            print(line.decode())
+            ct = time.time()
+            print("Current Time:", ct)
+            elasped_time = time.perf_counter() - self._start_time
+            print("Elasped Time: ", elasped_time)
+            ser.close()
 
-        lport = serialPorts2[selectedPort2-1]
+    def main():
 
-        with ProcessPoolExecutor() as executor:
-            p1 = executor.submit(receiveData, lport, 115200)
-            p2 = executor.submit(sendData, data, tty, 115200)
+        if __name__ == "__main__":
+            spObject = serial.tools.list_ports.comports()
 
-        print("Done!")
-        print()
+            serialPorts = []
+            print()
+            for i in spObject:
+                serialPorts.append(i.device)
+            print('Available ports:')
+            for i in range(len(serialPorts)):
+                print('%i. %s' % (i+1, serialPorts[i]))
+            selectedPort = int(input('Please select a port to send data to: '))
+            while selectedPort-1 not in range(len(serialPorts)):
+                print('Invalid input')
+                selectedPort = input('Please select a port: ')
+            tty = serialPorts[selectedPort-1]
+            data = input(
+                'Please enter the data you want to send over the serial port: ')
+            print()
+
+            spObject2 = serial.tools.list_ports.comports()
+            serialPorts2 = []
+            for i in spObject2:
+                serialPorts2.append(i.device)
+            print('Available ports:')
+            for i in range(len(serialPorts2)):
+                print('%i. %s' % (i+1, serialPorts2[i]))
+            selectedPort2 = int(input('Please select a port to listen on: '))
+            while selectedPort2-1 not in range(len(serialPorts2)):
+                print('Invalid input')
+                selectedPort2 = input('Please select a port: ')
+
+            lport = serialPorts2[selectedPort2-1]
+
+            with ProcessPoolExecutor() as executor:
+                p1 = executor.submit(receiveData, lport, 115200)
+                p2 = executor.submit(sendData, data, tty, 115200)
+
+            print("Done!")
+            print()
 main()
